fd2b33ed43408ec768a7f1ca16c396c5b42a0128
shelvy <sks91@berkeley.edu>
2021-11-04 13:11:09 -0700

    server heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   const [note, setNote] = useState('');
+  const [allNotes, setAllNotes] = useState([]);
+  useEffect(() => {
+    axios.get('/api/notes').then((res) => {
+      const newNotes = [];
+      res.data.forEach((data) => {
+        newNotes.push(data.note);
+      });
+      setAllNotes(newNotes);
+    });
+  }, []);
   console.log(note);
   const handleSubmit = (e) => {
     e.preventDefault();
     //do something with new note
-
+    //make post request to update DB
+    axios
+      .post('/api/notes', {
+        note: note,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+    setAllNotes((prev) => [...prev, note]);
     //make input fields clear again
   };
   return (
@@ -21,6 +40,12 @@ function App() {
         ></textarea>
         <button>Add note</button>
       </form>
+      <div className='noteslist'>
+        <h1>all the notes so far:</h1>
+        {allNotes.map((anote) => (
+          <div>{anote}</div>
+        ))}
+      </div>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 // Use API routes from the api folder
 const apis = require('./api');
 app.use('/api', apis);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 mongoose
   .connect(process.env.DB, {
